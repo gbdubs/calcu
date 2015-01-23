e7e7e59d0aee19a3ff553f10992b7e14044c3f65
gbdubs <gward@brandeis.edu>
2015-01-23 17:51:14 -0500

    Added in some simple functionality to the Achievments API that will make it easy to display them.

--- src/calculus/api/AchievmentsAPI.java
+++ src/calculus/api/AchievmentsAPI.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import calculus.models.Achievment;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 
 public class AchievmentsAPI {
 
@@ -48,4 +52,41 @@ public class AchievmentsAPI {
 		datastore.put(userPrivateInfo);
 	}
 	
+	public static List<Achievment> getUserAchievments(String userId){
+		return getAchievments(getUserAchievmentUuids(userId));
+	}
+	
+	public static List<Achievment> getUserUnfinishedAchievments(String userId){
+		return getAchievments(getUnfinishedAchievementUuids(userId));
+	}
+	
+	public static List<Achievment> getAchievments(List<String> uuids){
+		List<Achievment> achievments = new ArrayList<Achievment>();
+		for (String uuid : uuids){
+			achievments.add(new Achievment(uuid));
+		}
+		return achievments;
+	}
+	
+	private static List<String> getUserAchievmentUuids(String userId){
+		Entity userPrivateInfo = UserPrivateInfoAPI.getOrCreateUserPrivateInfo(userId);
+		List<Achievment> achievments = new ArrayList<Achievment>();
+		return (List<String>) userPrivateInfo.getProperty("achievments");
+	}
+	
+	private static List<String> getUnfinishedAchievementUuids(String userId){
+		List<String> allAchievments = getAllAchievmentUuids();
+		allAchievments.removeAll(getUserAchievmentUuids(userId));
+		return allAchievments;
+	}
+
+	private static List<String> getAllAchievmentUuids() {
+		List<String> result = new ArrayList<String>();
+		Query query = new Query("Achievment");
+		PreparedQuery pq = datastore.prepare(query);
+		for(Entity entity : pq.asIterable()){
+			result.add((String) entity.getProperty("uuid"));
+		}
+		return result;
+	}
 }
