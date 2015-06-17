2352edd27d87209337eecd2f6701b0dd4bda16e1
Grady Ward <gward@brandeis.edu>
2015-06-17 11:30:30 -0400

    Finally minimized my CSS and JS (installed Apache Ant on the Vertica Computers)

--- war/_static/dist/js/calcujs.js
+++ war/_static/dist/js/calcujs.js
@@ -1956,6 +1956,25 @@ $(window).load(function() {
     }
 })(window.jQuery || window.Zepto);
 $(function() {
+    "use strict";
+
+    if ($("#page-js").text().indexOf("baseline-landing") > -1) {
+    	$("#what-to-expect-button").click(function(){
+    		$("#what-to-expect-text").toggleClass("hidden");
+    		if (! $("#about-the-science-text").hasClass("hidden")){
+    			$("#about-the-science-text").addClass("hidden")
+    		}
+    	});
+    	
+    	$("#about-the-science-button").click(function(){
+    		$("#about-the-science-text").toggleClass("hidden");
+    		if (! $("#what-to-expect-text").hasClass("hidden")){
+    			$("#what-to-expect-text").addClass("hidden")
+    		}
+    	});
+    }
+    
+});$(function() {
 	"use strict";
 	
 	 if ($("#page-js").text().indexOf("compare-content") > -1) {
