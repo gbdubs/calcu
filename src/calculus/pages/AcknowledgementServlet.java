966909c81447974bad81cb15c0d967c89a0b7f5a
Roger LaCroix <rogrox1620@gmail.com>
2015-07-13 15:32:38 -0400

    Created the acknowledgements page jsp, along with the Servlet, url pattern, and mappings for it.

---
+++ src/calculus/pages/AcknowledgementServlet.java
@@ -0,0 +1,25 @@
+package calculus.pages;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import calculus.api.LandingFactsAPI;
+import calculus.api.UserContextAPI;
+
+import com.google.appengine.api.datastore.Entity;
+
+@SuppressWarnings("serial")
+public class AcknowledgementServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		
+		UserContextAPI.addUserContextToRequest(req, "/thankyou");
+		resp.setContentType("text/html");
+		RequestDispatcher jsp = req.getRequestDispatcher("/WEB-INF/pages/acknowledgements.jsp");
+		jsp.forward(req, resp);
+	}
+}
