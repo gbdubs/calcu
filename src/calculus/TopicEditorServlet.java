b56949c021e3f4e4420584b3f99c1bde9204bce3
Russell Santos <russellandshira@msn.com>
2015-07-22 15:08:53 -0400

    topic-editor addition

--- src/calculus/TopicEditorServlet.java
+++ src/calculus/TopicEditorServlet.java
@@ -1,27 +1,5 @@
 package calculus;
 
-import java.io.IOException;
+public class TopicEditorServlet {
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import calculus.api.LandingFactsAPI;
-import calculus.api.UserContextAPI;
-
-import com.google.appengine.api.datastore.Entity;
-
-@SuppressWarnings("serial")
-public class TopicEditorServlet extends HttpServlet {
-
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		
-		UserContextAPI.addUserContextToRequest(req, "/home");
-		resp.setContentType("text/html");
-		RequestDispatcher jsp = req.getRequestDispatcher("/WEB-INF/pages/topic-editor.jsp");
-		jsp.forward(req, resp);
-	}
-	
 }
