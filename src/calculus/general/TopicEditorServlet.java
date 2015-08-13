0c1a6b073582af401f891d38444476693281776d
Grady Ward <gward@brandeis.edu>
2015-08-13 17:01:00 -0400

    Topic Editor polishes/fixes

--- src/calculus/general/TopicEditorServlet.java
+++ src/calculus/general/TopicEditorServlet.java
@@ -1,4 +1,4 @@
-package calculus.topic;
+package calculus.general;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -50,7 +50,6 @@ public class TopicEditorServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		boolean isAdmin = userService.isUserLoggedIn() && userService.isUserAdmin();
-		
 		if (req.getParameter("action").equals("merge")){
 			String topicUuid1 = req.getParameter("targetTopic");
 			String topicUuid2 = req.getParameter("sourceTopic");
@@ -60,13 +59,16 @@ public class TopicEditorServlet extends HttpServlet {
 			} else {
 				resp.getWriter().println("FAILED TO MERGE TOPICS.");
 			}
-		} else if (req.getParameter("action").equals("update")){
+		} else if (req.getParameter("action").equals("Save")){
 			String uuid = UuidTools.getUuidFromUrl(req.getRequestURI());
+			if (uuid == null){
+				uuid = req.getParameter("uuid");
+			}
 			if (uuid != null && isAdmin){
 				try {
 					Topic t = new Topic(uuid);
-					List<String> subTopics = Arrays.asList(req.getParameter("subTopics").split(","));
-					List<String> parentTopics = Arrays.asList(req.getParameter("subTopics").split(","));
+					List<String> subTopics = Arrays.asList(req.getParameter("subTopics").replace("[", "").replace("]","").split(","));
+					List<String> parentTopics = Arrays.asList(req.getParameter("parentTopics").replace("[", "").replace("]","").split(","));
 					String title = req.getParameter("title");
 					String shortDescription = req.getParameter("shortDescription");
 					String longDescription = req.getParameter("longDescription");
@@ -90,6 +92,8 @@ public class TopicEditorServlet extends HttpServlet {
 					// Throw the not found page.
 				}
 			}
+		} else if (req.getParameter("action").equals("Delete")){
+			
 		}
 		UserContextAPI.addUserContextToRequest(req, req.getRequestURI());
 		resp.setContentType("text/html");
